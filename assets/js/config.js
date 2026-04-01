db342161e2f3ef0537a7230cec358d345e45c142
tnaseemudheen <voidcorecomp@gmail.com>
2026-04-02 02:47:46 +0530

    Delete assets/js directory

--- assets/js/config.js
+++
@@ -1,7 +0,0 @@
-// assets/js/config.js
-window.CONFIG = {
-  API_KEY: "AIzaSyC3r5fRGVLU5yz-xhoa0qCryXWii6hShCs",
-  CHANNEL_ID: "UCnjj2xxgWwjUT2RMlg8Ea7A",
-  LOGO: "assets/img/logo.png",
-  DEFAULT_MAX: 12
-};
